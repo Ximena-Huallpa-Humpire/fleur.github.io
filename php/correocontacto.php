78cf4054338d7b223d4c3c356c646cf9bf3394dd
Ximena-Huallpa-Humpire <xim.hopeh@gmail.com>
2020-09-28 23:47:16 -0500

    Create correocontacto.php
    v2

---
+++ php/correocontacto.php
@@ -0,0 +1,15 @@
+<?php
+
+$destinatario = 'ximhopeh@gmail.com'
+$nombre = $_POST['Nombre'];
+$mensaje = $_POST['Mensaje'];
+$correo = $_POST['Correo'];
+
+$header = "Enviado desde FLEUR";
+$mensajecompleto = $mensaje . "\nAtentamente: " . $nombre
+	
+mail($destinatario, $mensajecompleto, $header);
+echo "<script>alert('Mensaje enviado exitosamente')</script>";
+echo "<script> setTimeout(\"location.href='index.html'\", 1000)</script>";
+
+?>
